d93ca960f2a383a6a98d6c18b0b618ffea6d3f6b
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-06 18:16:10 -0500

    Template FormsModule

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class BasicosComponent {
 
+
+  guardar(miFormulario: NgForm){
+    console.log(miFormulario.value);
+  }
 }
