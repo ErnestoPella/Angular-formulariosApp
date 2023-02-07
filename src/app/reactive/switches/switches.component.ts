0a5bb54905f885d26d5ed9ef8496468608debac3
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-07 16:55:33 -0500

    Formulario reactivo switches creado

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit{
+
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]
+  });
+
+  persona ={
+    genero: 'F',
+    notificaciones: true
+  }
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false
+    });
+  }
 
 }
