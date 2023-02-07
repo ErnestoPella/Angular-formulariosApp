62798eef57b886578302e940d2dd2c810fa64fba
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-06 20:58:53 -0500

    Template formularios dinámicos y arreglos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class DinamicosComponent {
 
+  @ViewChild('dinamicForm') dinamicForm!: NgForm;
+
+
+  nombreValido():boolean{
+    return this.dinamicForm?.controls['nombre']?.invalid;       
+  }
+
+  guardar(){
+
+  }
 }
