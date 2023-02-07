bd34554448c3cd81922826514830430fa6d7587e
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-07 14:12:55 -0500

    Formulario reactivo básico inicio

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./basicos.component.css']
 })
 export class BasicosComponent {
+
+  miFormulario: FormGroup = new FormGroup({
+    'nombre': new FormControl('RTX 4080ti')
+  });
+
+  constructor(){}
 
 }
