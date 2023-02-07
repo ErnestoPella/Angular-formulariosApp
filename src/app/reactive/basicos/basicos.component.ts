9a059212d44b9146485b33206bf07aef87805e43
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-07 14:39:01 -0500

    Mostrar mensaje

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -15,11 +15,16 @@ export class BasicosComponent {
   });*/
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: [ 'RTX 4080ti'],
-    precio: [ 0 ],
-    existencias: [ 0 ]
+    nombre: [ '', [Validators.required, Validators.minLength(3)]],
+    precio: [ 0, [Validators.required, Validators.min(0)] ],
+    existencias: [ 0, [Validators.required, Validators.min(0)] ]
   })
 
   constructor(private fb: FormBuilder){}
 
+
+  campoNoEsValido( campo: string ){
+    return this.miFormulario.controls[campo].errors
+    && this.miFormulario.controls[campo].touched;
+  }
 }
