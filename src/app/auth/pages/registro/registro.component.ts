91c2c592d5ab1cc04411f42853ef1c81624c479b
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-08 17:28:37 -0500

    Validar contra una expresión regular

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class RegistroComponent {
 
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+  })
+
+
+  constructor(private fb: FormBuilder){}
+
+
+  campoNoValido(campo: string){
+    return this.miFormulario.get(campo)?.invalid
+            && this.miFormulario.get(campo)?.touched;
+  }
+
+
+  submitFormulario(){
+
+    this.miFormulario.markAllAsTouched();
+  }
 }
