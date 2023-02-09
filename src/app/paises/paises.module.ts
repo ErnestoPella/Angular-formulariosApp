d87da56cc62572fabecf4473487efc30ef49a4ac
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-09 16:09:09 -0500

    Formulario reactivo de selector de países

--- src/app/paises/paises.module.ts
+++ src/app/paises/paises.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { ReactiveRoutingModule } from '../reactive/reactive-routing.module';
 import { PaisesRoutingModule } from './paises-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +13,8 @@ import { PaisesRoutingModule } from './paises-routing.module';
   ],
   imports: [
     CommonModule,
-    PaisesRoutingModule
+    PaisesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class PaisesModule { }
