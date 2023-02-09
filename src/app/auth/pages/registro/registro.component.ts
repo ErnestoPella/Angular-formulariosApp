cceee8779dc64e01458f89728283565ec6ad5354
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-08 19:30:32 -0500

    Validación asincrónica y estado del formulario

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { emailPattern, nombreApellidoPattern, noPuedeSerStrider } from 'src/app/shared/validator/validaciones';
 import { ValidatorService } from '../../../shared/validator/validator.service';
+import { EmailValidatorService } from '../../../shared/validator/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -13,7 +14,7 @@ export class RegistroComponent implements OnInit{
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.validatorsService.nombreApellidoPattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.validatorsService.noPuedeSerStrider]],
     password: ['',[Validators.required, Validators.minLength(6)]],
     password2:['',[Validators.required]] 
@@ -23,13 +24,16 @@ export class RegistroComponent implements OnInit{
 
 
   constructor(private fb: FormBuilder,
-              private validatorsService: ValidatorService){}
+              private validatorsService: ValidatorService,
+              private emailValidator: EmailValidatorService){}
 
   ngOnInit(): void {
     this.miFormulario.reset({
       nombre: 'Ernesto Pella',
       email: 'example@gmail.com',
-      username: 'EPella'
+      username: 'EPella',
+      password: '123456',
+      password2: '123456'
     })
   }
 
